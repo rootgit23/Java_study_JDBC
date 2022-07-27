42963525327ffc0e22c04264270730dfa8ebe5c0
rootgit23 <rmsgh1009@naver.com>
2022-07-27 12:20:40 +0900

    test

--- src/com/im/test/TestMain.java
+++ src/com/im/test/TestMain.java
@@ -1,9 +1,17 @@
 package com.im.test;
 
+import com.im.util.DBConnect;
+
 public class TestMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		DBConnect dbConnector = new DBConnect();
+		try {
+			dbConnector.getConnection();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
